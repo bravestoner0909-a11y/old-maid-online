ca54a764b7e00f771ad25bf823b331c128baecf4
bravestoner0909-a11y <bravestoner0909@gmail.com>
2026-05-24 23:19:15 +0900

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Fill these two values from Supabase Project Settings → API.
 // The anon public key is safe to use in browser apps when RLS/policies are configured.
 window.OLD_MAID_SUPABASE_URL = "https://lxuhimbbaxomtyxhdhvc.supabase.co";
-window.OLD_MAID_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4dWhpbWJiYXhvbXR5eGhkaHZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MTYyOTgsImV4cCI6MjA5NTE5MjI5OH0.NvaCvbctbimQYaBryL5E3hSo0uYE8ftkjbQ8rP_-Tmk";
+window.OLD_MAID_SUPABASE_ANON_KEY = "sb_publishable_yVtPgD8KFRbD_mcJJqiwrQ_e0DMdf0Q";
